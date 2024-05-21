d3d6f2e38841ec0ab721514f741023dc5cc0b11e
Onur Erdinc <ONURERDNC@GMAIL.COM>
2024-05-21 10:28:25 +0300

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let initialCards = [
+const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
@@ -55,6 +55,7 @@ function getCardElement(cardData) {
 
   cardTitleElement.textContent = cardData.name;
   cardImageElement.src = cardData.link;
+  cardImageElement.alt = `Photo of ${cardData.name}`;
   return cardElement;
 }
 /* Event Handlers */
