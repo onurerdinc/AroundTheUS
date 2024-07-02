ee1f1c0e8e3edb87bb8d39496d0d99e9d23c0adb
Onur Erdinc <ONURERDNC@GMAIL.COM>
2024-07-02 08:50:43 +0300

    add form validation

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,13 +1,31 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 //Functions
+
+function setEventListeners(formEl, options) {
+  const { inputSelector } = options;
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  console.log(inputEls);
+}
+
 function enableValidation(options) {
-  const formEls = [...document.querySelectorAll(".popup_form")];
-  formEls.forEach((formEl) =>
+  const formEls = [...document.querySelectorAll(options.formSelector)];
+  formEls.forEach((formEl) => {
     formEl.addEventListener("submit", (e) => {
       e.preventDefault();
-    })
-  );
+    });
+
+    setEventListeners(formEl, options);
+    // loof for all the inputs inside the forms
+    // see if they are valid
+    // if any input is not valid
+    // get validation message
+    // add error class to input
+    // display error message
+    // disable the button
+    // if all the inputs valid, enable button
+    // reset error messages
+  });
 }
 
 // Variables
