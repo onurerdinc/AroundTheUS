f298600734bb802559be7e6517da30f4acf883c0
Onur Erdinc <ONURERDNC@GMAIL.COM>
2024-07-03 15:11:47 +0300

    add form validity

--- scripts/validation.js
+++ scripts/validation.js
@@ -2,14 +2,60 @@
 // pass all the settings on call
 //Functions
 
+function showInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.add(inputErrorClass);
+  errorMessageEl.textContent = inputEl.validationMessage;
+  errorMessageEl.classList.add(errorClass);
+}
+
+function hideInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.remove(inputErrorClass);
+  errorMessageEl.textContent = "";
+  errorMessageEl.classList.remove(errorClass);
+}
+
+function checkInputValidity(formEl, inputEl, options) {
+  if (!inputEl.validity.valid) {
+    showInputError(formEl, inputEl, options);
+  } else {
+    hideInputError(formEl, inputEl, options);
+  }
+}
+
+function toggleButtonState(inputEls, submitButton, { inactiveButtonClass }) {
+  let foundInvalid = false;
+  inputEls.forEach((inputEl) => {
+    if (!inputEl.validity.valid) {
+      foundInvalid = true;
+    }
+    if (foundInvalid) {
+      submitButton.disabled = true;
+      submitButton.classList.add(inactiveButtonClass);
+    }
+    {
+      submitButton.classList.remove(inactiveButtonClass);
+      submitButton.disabled = false;
+    }
+  });
+}
+
 function setEventListeners(formEl, options) {
   const { inputSelector } = options;
   const inputEls = [...formEl.querySelectorAll(inputSelector)];
-  console.log(inputEls);
+  const submitButton = formEl.querySelectorAll(".modal__save-button");
+  inputEls.forEach((inputEl) => {
+    inputEl.addEventListener("input", (e) =>
+      checkInputValidity(formEl, inputEl, options)
+    );
+    toggleButtonState(inputEls, submitButton, { inactiveButtonClass });
+  });
+  toggleButtonState(inputEls, submitButton, inactiveButtonClass);
 }
 
 function enableValidation(options) {
-  const formEls = [...document.querySelectorAll(options.formSelector)];
+  const formEls = Array.from(document.querySelectorAll(options.formSelector));
   formEls.forEach((formEl) => {
     formEl.addEventListener("submit", (e) => {
       e.preventDefault();
@@ -31,12 +77,12 @@ function enableValidation(options) {
 // Variables
 
 const config = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__error_visible",
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__save-button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
 };
 
 enableValidation(config);
